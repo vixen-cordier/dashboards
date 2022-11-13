d1925a9480034ab6f4522d906ad2be554c0c3ba2
VCO <vincent.cordier@alstefgroup.com>
2022-11-13 18:45:11 +0100

    first commit

---
+++ portfolio.py
@@ -0,0 +1,9 @@
+import gspread as gs
+import pandas as pd
+
+gc = gs.service_account(filename='cred-api-sheets.json')
+
+ss_portfolio_key = '1xhq1RGQLcuUVr88-5Gl4tEvY0eI5M_i8TkkzRZwSC80'
+ss_portfolio = gc.open_by_key(ss_portfolio_key).worksheet("Operations")
+df_portfolio = pd.DataFrame(ss_portfolio.get_all_records())
+df_portfolio
