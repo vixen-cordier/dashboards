13a33334d9e8192a11687bd2eab76a3e91925944
VCO <vixen.cordier.13@gmail.com>
2023-01-18 06:02:18 +0100

    use_container_width=True & try to delete methode

--- app.py
+++ app.py
@@ -16,121 +16,99 @@ print("""
 # ----------------------------------------------
 """)
 
-def extract_sheets():
-    gc = gs.service_account_from_dict(st.secrets['gcp_service_account'])
-    ss = gc.open_by_key(st.secrets['portfolio'].spreadsheet_key)
-    dicts = pd.DataFrame(ss.worksheet('Dict').get_all_records())
-    return {
-        'Operation': pd.DataFrame(
-            ss.worksheet('Operations').get_all_records()
-            ).sort_values('Date').astype({'Date': 'datetime64[ns]'}).set_index('Date')
-        ,
-        'Others': {
-            'GREENBULL': pd.DataFrame(
-                ss.worksheet('GREENBULL').get_all_records()
-                ).sort_values('Date').astype({'Date': 'datetime64[ns]'}).set_index('Date')
-        },
-        'Dicts': {
-            'assets': pd.DataFrame(dicts[['Asset', 'Market', 'Currency']].set_index('Asset').to_dict()),
-            'depots': pd.DataFrame(dicts[['Depot', 'Forex']].set_index('Depot').to_dict())
-        }
-    }
+gc = gs.service_account_from_dict(st.secrets['gcp_service_account'])
+ss = gc.open_by_key(st.secrets['portfolio'].spreadsheet_key)
+dicts = pd.DataFrame(ss.worksheet('Dict').get_all_records())
+operation = pd.DataFrame(ss.worksheet('Operations').get_all_records()).sort_values('Date').astype({'Date': 'datetime64[ns]'}).set_index('Date')
+greenbull = pd.DataFrame(ss.worksheet('GREENBULL').get_all_records()).sort_values('Date').astype({'Date': 'datetime64[ns]'}).set_index('Date')
+assets = pd.DataFrame(dicts[['Asset', 'Market', 'Currency']].set_index('Asset').to_dict()),
+depots = pd.DataFrame(dicts[['Depot', 'Forex']].set_index('Depot').to_dict())
+
+print(list(assets['Market']))
+print(depots)
+
+market = yf.download(' '.join(list(assets['Market'])[:-1]), start='2021-04-01')['Close']
+market = pd.concat([market, greenbull], axis=1)
+market = pd.concat([market], keys=['Market'], axis=1)
+market = pd.concat([market], keys=['Cotation'], axis=1)
+
+forex: pd.DataFrame = yf.download(' '.join(list(depots['Forex'])), start='2021-04-01')['Close']
+forex = pd.concat([forex], keys=['Forex'], axis=1)
+forex = pd.concat([forex], keys=['Cotation'], axis=1)
 
 
-def fetch_market(assets, others):
-    market = yf.download(' '.join(list(assets['Market'])[:-1]), start='2021-04-01')['Close']
-    market = pd.concat([market, others['GREENBULL']], axis=1)
-    market = pd.concat([market], keys=['Market'], axis=1)
-    market = pd.concat([market], keys=['Cotation'], axis=1)
-    return market
+df = pd.concat([market, forex], axis=1).ffill().bfill()
+df['DepositEUR', 'All', 'All'] = 0
+df['InvestedEUR', 'All', 'All'] = 0
+df['ValueEUR', 'All', 'All'] = 0
+df['PnLEUR', 'All', 'All'] = 0
+df['CashEUR', 'All', 'All'] = 0
 
-def fetch_forex(depots):
-    forex: pd.DataFrame = yf.download(' '.join(list(depots['Forex'])), start='2021-04-01')['Close']
-    forex = pd.concat([forex], keys=['Forex'], axis=1)
-    forex = pd.concat([forex], keys=['Cotation'], axis=1)
-    return forex
+for portfolio in np.unique(operation['Portfolio']):
+    dfp: pd.DataFrame = operation[operation['Portfolio'] == portfolio]
+    print('', portfolio)
 
+    for asset in np.unique(dfp['Asset']):
+        dfa: pd.DataFrame = dfp[dfp['Asset'] == asset]
+        dfa = dfa.groupby('Date').agg({'Amount': "sum", 'Total': "sum"})
+        print('  ', asset)
 
-def build_metrics(assets, market, depots, forex, operation):
-    df = pd.concat([market, forex], axis=1).ffill().bfill()
-    df['DepositEUR', 'All', 'All'] = 0
-    df['InvestedEUR', 'All', 'All'] = 0
-    df['ValueEUR', 'All', 'All'] = 0
-    df['PnLEUR', 'All', 'All'] = 0
-    df['CashEUR', 'All', 'All'] = 0
+        amt, tot = pd.Series(dtype=float), pd.Series(dtype=float)
+        for idx in dfa.index:
+            amt[idx] = np.sum(dfa.loc[:idx]['Amount'])
+            tot[idx] = np.sum(dfa.loc[:idx]['Total'])
 
-    for portfolio in np.unique(operation['Portfolio']):
-        dfp: pd.DataFrame = operation[operation['Portfolio'] == portfolio]
-        print('', portfolio)
+        if asset in assets['Market'].keys():
+            df = pd.concat([df, pd.DataFrame({
+                ('Amount', portfolio, asset): amt,
+                ('Invested', portfolio, asset): tot,
+            })], axis=1).ffill().fillna(0)
 
-        for asset in np.unique(dfp['Asset']):
-            dfa: pd.DataFrame = dfp[dfp['Asset'] == asset]
-            dfa = dfa.groupby('Date').agg({'Amount': "sum", 'Total': "sum"})
-            print('  ', asset)
+            df['InvestedEUR', portfolio, asset] = df['Invested', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][assets['Currency'][asset]]]
+            df['PRU', portfolio, asset] = df['Invested', portfolio, asset] / df['Amount', portfolio, asset]
+            df['Value', portfolio, asset] = df['Amount', portfolio, asset] * df['Cotation', 'Market', assets['Market'][asset]]
+            df['ValueEUR', portfolio, asset] = df['Value', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][assets['Currency'][asset]]]
+            df['PnL', portfolio, asset] = df['Value', portfolio, asset] - df['Invested', portfolio, asset]
+            df['PnLEUR', portfolio, asset] = df['PnL', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][assets['Currency'][asset]]]
 
-            amt, tot = pd.Series(dtype=float), pd.Series(dtype=float)
-            for idx in dfa.index:
-                amt[idx] = np.sum(dfa.loc[:idx]['Amount'])
-                tot[idx] = np.sum(dfa.loc[:idx]['Total'])
+        elif asset in depots['Forex'].keys():
+            df = pd.concat([df, pd.DataFrame({
+                ('Amount', portfolio, asset): amt,
+                ('Deposit', portfolio, asset): tot,
+            })], axis=1).ffill().fillna(0)
 
-            if asset in assets['Market'].keys():
-                df = pd.concat([df, pd.DataFrame({
-                    ('Amount', portfolio, asset): amt,
-                    ('Invested', portfolio, asset): tot,
-                })], axis=1).ffill().fillna(0)
+            df['DepositEUR', portfolio, asset] = df['Deposit', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][asset]]
+            # df['PnL', portfolio, asset] = df['Deposit', portfolio, asset] - df['DepositEUR', portfolio, asset]
+            # df['PnLEUR', portfolio, asset] = df['PnL', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][asset]]
+            
 
-                df['InvestedEUR', portfolio, asset] = df['Invested', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][assets['Currency'][asset]]]
-                df['PRU', portfolio, asset] = df['Invested', portfolio, asset] / df['Amount', portfolio, asset]
-                df['Value', portfolio, asset] = df['Amount', portfolio, asset] * df['Cotation', 'Market', assets['Market'][asset]]
-                df['ValueEUR', portfolio, asset] = df['Value', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][assets['Currency'][asset]]]
-                df['PnL', portfolio, asset] = df['Value', portfolio, asset] - df['Invested', portfolio, asset]
-                df['PnLEUR', portfolio, asset] = df['PnL', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][assets['Currency'][asset]]]
+    if 'DepositEUR' in df.columns and portfolio in df['DepositEUR'].columns:
+        df['DepositEUR', portfolio, 'All'] = df['DepositEUR', portfolio].sum(axis=1)
+    else:
+        df['DepositEUR', portfolio, 'All'] = 0
+    df['DepositEUR', 'All', 'All'] += df['DepositEUR', portfolio, 'All']
 
-            elif asset in depots['Forex'].keys():
-                df = pd.concat([df, pd.DataFrame({
-                    ('Amount', portfolio, asset): amt,
-                    ('Deposit', portfolio, asset): tot,
-                })], axis=1).ffill().fillna(0)
+    if 'InvestedEUR' in df.columns and portfolio in df['InvestedEUR'].columns:
+        df['InvestedEUR', portfolio, 'All'] = df['InvestedEUR', portfolio].sum(axis=1)
+    else:
+        df['InvestedEUR', portfolio, 'All'] = 0
+    df['InvestedEUR', 'All', 'All'] += df['InvestedEUR', portfolio, 'All']
+    
+    if 'ValueEUR' in df.columns and portfolio in df['ValueEUR'].columns:
+        df['ValueEUR', portfolio, 'All'] = df['ValueEUR', portfolio].sum(axis=1)
+    else:
+        df['ValueEUR', portfolio, 'All'] = 0
+    df['ValueEUR', 'All', 'All'] += df['ValueEUR', portfolio, 'All']
 
-                df['DepositEUR', portfolio, asset] = df['Deposit', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][asset]]
-                # df['PnL', portfolio, asset] = df['Deposit', portfolio, asset] - df['DepositEUR', portfolio, asset]
-                # df['PnLEUR', portfolio, asset] = df['PnL', portfolio, asset] * df['Cotation', 'Forex', depots['Forex'][asset]]
-                
+    if 'PnLEUR' in df.columns and portfolio in df['PnLEUR'].columns:
+        df['PnLEUR', portfolio, 'All'] = df['PnLEUR', portfolio].sum(axis=1)
+    else:
+        df['PnLEUR', portfolio, 'All'] = 0
+    df['PnLEUR', 'All', 'All'] += df['PnLEUR', portfolio, 'All']
 
-        if 'DepositEUR' in df.columns and portfolio in df['DepositEUR'].columns:
-            df['DepositEUR', portfolio, 'All'] = df['DepositEUR', portfolio].sum(axis=1)
-        else:
-            df['DepositEUR', portfolio, 'All'] = 0
-        df['DepositEUR', 'All', 'All'] += df['DepositEUR', portfolio, 'All']
+    df['CashEUR', portfolio, 'All'] = df['DepositEUR', portfolio, 'All'] - df['InvestedEUR', portfolio, 'All']
+    df['CashEUR', 'All', 'All'] += df['CashEUR', portfolio, 'All']
 
-        if 'InvestedEUR' in df.columns and portfolio in df['InvestedEUR'].columns:
-            df['InvestedEUR', portfolio, 'All'] = df['InvestedEUR', portfolio].sum(axis=1)
-        else:
-            df['InvestedEUR', portfolio, 'All'] = 0
-        df['InvestedEUR', 'All', 'All'] += df['InvestedEUR', portfolio, 'All']
-        
-        if 'ValueEUR' in df.columns and portfolio in df['ValueEUR'].columns:
-            df['ValueEUR', portfolio, 'All'] = df['ValueEUR', portfolio].sum(axis=1)
-        else:
-            df['ValueEUR', portfolio, 'All'] = 0
-        df['ValueEUR', 'All', 'All'] += df['ValueEUR', portfolio, 'All']
-
-        if 'PnLEUR' in df.columns and portfolio in df['PnLEUR'].columns:
-            df['PnLEUR', portfolio, 'All'] = df['PnLEUR', portfolio].sum(axis=1)
-        else:
-            df['PnLEUR', portfolio, 'All'] = 0
-        df['PnLEUR', 'All', 'All'] += df['PnLEUR', portfolio, 'All']
-
-        df['CashEUR', portfolio, 'All'] = df['DepositEUR', portfolio, 'All'] - df['InvestedEUR', portfolio, 'All']
-        df['CashEUR', 'All', 'All'] += df['CashEUR', portfolio, 'All']
-
-    return df
-
-
-sheets = extract_sheets()
-market = fetch_market(sheets['Dicts']['assets'], sheets['Others'])
-forex = fetch_forex(sheets['Dicts']['depots'])
-data = build_metrics(sheets['Dicts']['assets'], market, sheets['Dicts']['depots'], forex, sheets['Operation'])
 
 
 print("""
@@ -155,7 +133,7 @@ button[data-baseweb="tab"] > div[data-testid="stMarkdownContainer"] > p {
 
 all_tab, zen_tab, dma_tab = st.tabs(["Overview", "ZEN", "DMA"])
 
-df = data[['ValueEUR', 'InvestedEUR', 'PnLEUR', 'CashEUR', 'DepositEUR']]
+data = df[['ValueEUR', 'InvestedEUR', 'PnLEUR', 'CashEUR', 'DepositEUR']]
 s = df.iloc[-1]
 
 with all_tab:
@@ -167,41 +145,43 @@ with all_tab:
     st.dataframe(df_all.transpose().style.format("{:.0f}"), use_container_width=True)
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.index, y=df['ValueEUR', 'All', 'All'], name='Value'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['InvestedEUR', 'All', 'All'], name='Invested'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['PnLEUR', 'All', 'All'], name='PnL'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['CashEUR', 'All', 'All'], name='Cash'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['DepositEUR', 'All', 'All'], name='Deposit'))
-    st.plotly_chart(fig)
+    fig.add_trace(go.Scatter(x=data.index, y=data['ValueEUR', 'All', 'All'], name='Value'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['InvestedEUR', 'All', 'All'], name='Invested'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['PnLEUR', 'All', 'All'], name='PnL'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['CashEUR', 'All', 'All'], name='Cash'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['DepositEUR', 'All', 'All'], name='Deposit'))
+    fig.update_layout(autosize=True)
+    # with st.container():
+    st.plotly_chart(fig, use_container_width=True)
 
     st.header("Portfolio repartition")
     pie_col, _, lin_col = st.columns([2, 1, 4])
     with pie_col:
         df_all = pd.concat({ 
-            'ZEN' : df['ValueEUR', 'ZEN', 'All'],  
-            'DMA' : df['ValueEUR', 'DMA', 'All'],  
-            'Cash' : df['CashEUR', 'All', 'All']
+            'ZEN' : data['ValueEUR', 'ZEN', 'All'],  
+            'DMA' : data['ValueEUR', 'DMA', 'All'],  
+            'Cash' : data['CashEUR', 'All', 'All']
         }, axis=1) 
         s_all = df_all.iloc[-1]
         fig = go.Figure(go.Pie(values=s_all.values, labels=s_all.index))
-        st.plotly_chart(fig)
+        st.plotly_chart(fig, use_container_width=True)
     
     with lin_col:
         fig = go.Figure(go.Scatter(x=df_all.index, y=df_all['ZEN'], name='ZEN', stackgroup='one', groupnorm='percent'))
         fig.add_trace(go.Scatter(x=df_all.index, y=df_all['DMA'], name='DMA', stackgroup='one'))
         fig.add_trace(go.Scatter(x=df_all.index, y=df_all['Cash'], name='Cash', stackgroup='one'))
-        st.plotly_chart(fig)
+        st.plotly_chart(fig, use_container_width=True)
 
 
     st.header("Portfolio performance")
     df_all = pd.concat({ 
-        'ZEN' : df['PnLEUR', 'ZEN', 'All'],  
-        'DMA' : df['PnLEUR', 'DMA', 'All'],  
-        'All' : df['PnLEUR', 'All', 'All']
+        'ZEN' : data['PnLEUR', 'ZEN', 'All'],  
+        'DMA' : data['PnLEUR', 'DMA', 'All'],  
+        'All' : data['PnLEUR', 'All', 'All']
     }, axis=1) 
     s_all = df_all.iloc[-1]
     fig = go.Figure(go.Bar(x=s_all.index, y=s_all.values))
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 
 
@@ -221,13 +201,13 @@ def scatter_ptf(df, ptf):
     fig.add_trace(go.Scatter(x=df.index, y=df['PnLEUR', ptf, 'All'], name='PnL'))
     fig.add_trace(go.Scatter(x=df.index, y=df['CashEUR', ptf, 'All'], name='Cash'))
     fig.add_trace(go.Scatter(x=df.index, y=df['DepositEUR', ptf, 'All'], name='Deposit'))
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 def scatter_metric(df, ptf, metric):
     fig = go.Figure()
     for asset in df[metric, ptf].columns:
         fig.add_trace(go.Scatter(x=df.index, y=df[metric, ptf, asset], name=asset))
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 def pie_metric(s, ptf, metric):
     value = []
@@ -237,14 +217,14 @@ def pie_metric(s, ptf, metric):
             value.append(s[metric, ptf, asset])
             label.append(asset)
     fig = go.Figure(go.Pie(values=value, labels=label))
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 def stack_metric(df, ptf, metric):
     fig = go.Figure()
     for asset in df[metric, ptf].columns:
         if asset != 'All':
             fig.add_trace(go.Scatter(x=df.index, y=df[metric, ptf, asset], name=asset, stackgroup='one', groupnorm='percent'))
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 def bar_metric(s, ptf, metric):
     value = []
@@ -253,25 +233,25 @@ def bar_metric(s, ptf, metric):
         value.append(s[metric, ptf, asset])
         label.append(asset)
     fig = go.Figure(go.Bar(x=label, y=value))
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 
 with zen_tab:
-    datatable_ptf(s, 'ZEN')
-    scatter_ptf(df, 'ZEN')
+    datatable_ptf(data.iloc[-1], 'ZEN')
+    scatter_ptf(data, 'ZEN')
 
     st.header("ZEN Value")
-    scatter_metric(df, 'ZEN', 'ValueEUR')
+    scatter_metric(data, 'ZEN', 'ValueEUR')
 
     pie_col, lin_col = st.columns([1, 2])
     with pie_col:
-        pie_metric(s, 'ZEN', 'ValueEUR')
+        pie_metric(data.iloc[-1], 'ZEN', 'ValueEUR')
 
     with lin_col:
-        stack_metric(df, 'ZEN', 'ValueEUR')
+        stack_metric(data, 'ZEN', 'ValueEUR')
 
     st.header("ZEN Performance")
-    bar_metric(s, 'ZEN', 'PnLEUR')
+    bar_metric(data.iloc[-1], 'ZEN', 'PnLEUR')
 
 
 
