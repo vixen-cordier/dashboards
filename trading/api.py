89f92a3f51d70266664b41314792725c6de5b6da
VCO <vixen.cordier.13@gmail.com>
2023-04-03 06:00:44 +0200

    dev encours trading

--- trading/api.py
+++ trading/api.py
@@ -1,11 +1,53 @@
 import streamlit as st
 import gspread as gs 
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import warnings
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
-def build_data():
+
+def fetch_data():
     gc = gs.service_account_from_dict(st.secrets['gcp_service_account'])
     ss = gc.open_by_key(st.secrets['trading'].spreadsheet_key)
     trades = pd.DataFrame(ss.worksheet('Trades').get_values('A:AD'))
     trades = pd.DataFrame(trades.values[1:], columns=trades.iloc[0])
-    trades = trades.astype({'Date': 'datetime64[ns]', 'Gain': 'float64'}).sort_values('Date')
+    trades = trades.astype({'Date': 'datetime64[ns]', 'Gain': 'float64'})
+    trades = trades.sort_values('Date').reset_index(drop=True)[['Date', 'Gain', 'Result']]
     return trades
+
+
+def compute_stats(df: pd.DataFrame, all_stats=True):
+    trades = pd.DataFrame(index=df.index, columns=pd.MultiIndex(levels=[[],[]], codes=[[],[]]))
+    trades['Date', '.'] = df['Date']
+
+    for idx in trades.index:
+        globally = 'Globally'
+        yearly = f"Yearly_{df.loc[idx, 'Date'].year}"
+        monthly = f"Monthly_{df.loc[idx, 'Date'].year}-{df.loc[idx, 'Date'].month}"
+        weekly = f"Weekly_{df.loc[idx, 'Date'].year}-{df.loc[idx, 'Date'].week}"
+
+        for period in [globally, yearly, monthly, weekly]:
+            trades.loc[idx, ('Gain', period)] = df.loc[idx, 'Gain']
+            trades.loc[idx, ('Balance', period)] = np.sum(trades.loc[:idx, ('Gain', period)])
+            
+            if all_stats:
+                trades.loc[idx, ('Count', period)] = trades.loc[:idx, ('Gain', period)].count()
+
+                for result in ['SL', 'BE', 'TP']:
+                    if result in df.loc[idx, 'Result']:
+                        trades.loc[idx, (f'Gain{result}', period)] = trades.loc[idx, ('Gain', period)]
+                        trades.loc[idx, (f'Count{result}', period)] = trades.loc[:idx, (f'Gain{result}', period)].count()
+                        trades.loc[idx, (f'Rate{result}', period)] = trades.loc[idx, (f'Count{result}', period)] / trades.loc[idx, ('Count', period)]
+                        trades.loc[idx, (f'Balance{result}', period)] = np.sum(trades.loc[:idx, (f'Gain{result}', period)])
+                        trades.loc[idx, (f'Payoff{result}', period)] = trades.loc[idx, (f'Balance{result}', period)] / trades.loc[idx, (f'Count{result}', period)]
+
+    return trades
+
+
+def convert_to_time(df: pd.DataFrame):
+    return pd.concat([
+        pd.DataFrame({'Date': pd.date_range(start=df['Date'].min(), end=df['Date'].max())}).set_index('Date'),
+        df.set_index('Date').groupby('Date').agg({'Gain': 'sum'}),
+    ], axis=1)
+
--- trading/app.py
+++ trading/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 st.set_page_config(layout="wide", page_title="Trading results")
 import plotly.graph_objects as go
 
@@ -6,11 +7,66 @@ from api import *
 
 @st.experimental_memo 
 def get_data():
-    return build_data()
+    print("""
+    # ------------------------------------
+    # Fetch trades, build calculated trades
+    # for Trading Dashboard 
+    # ------------------------------------
+    """)
+    trades_unit = fetch_data()
+    trades_date = convert_to_time(trades_unit)
+    trades_unit = compute_stats(trades_unit)
+    trades_date = compute_stats(trades_date.reset_index(), all_stats=False).set_index(('Date', '.'))
+    return trades_unit, trades_date
 
-trades = get_data()
+trades_unit, trades_date = get_data()
 
+
+print("""
+    # ------------------------------------
+    # Build Streamlit Web page
+    # of Trading Dashboard 
+    # ------------------------------------
+""")
 st.title("Trading Dashboard")
 
-fig = go.Figure(go.Bar(x=trades.index.to_list(), y=trades['Gain'].to_list()))
+manage_axis, _, manage_graph = st.columns([5,1,5])
+
+with manage_axis:
+    st.subheader("Manage horizontal axis")
+    start_date = datetime(trades_date.index.min().year, trades_date.index.min().month, trades_date.index.min().day)
+    ended_date = datetime(trades_date.index.max().year, trades_date.index.max().month, trades_date.index.max().day)
+    date = st.slider("Slider to filter data", value=(start_date, ended_date), format="YY-MM-DD", label_visibility='collapsed')
+    axis = st.radio("Horizontal axis graph", ['per Trade', 'per Date'], horizontal=True, key="Horizontal axis", label_visibility='hidden')
+    if axis == 'per Trade':
+        trades = trades_unit.loc[(trades_unit['Date', '.'] > date[0]) & (trades_unit['Date', '.'] < date[1])]
+    elif axis == 'per Date':
+        trades = trades_date.loc[date[0]:date[1]]
+    else: 
+        print("Horizontal axis ERROR")
+
+with manage_graph:
+    st.subheader("Choose period results")
+    PERIOD = ['Globally', 'Yearly', 'Monthly', 'Weekly']
+    periods = []
+    for period in PERIOD:
+        if st.checkbox(period):
+            periods.append(period)
+        
+
+
+fig = go.Figure(go.Bar(x=trades.index.to_list(), y=trades[('Gain', 'Globally')].to_list(), name='Trades'))
+for col in trades['Balance'].columns.get_level_values(0):
+    for period in periods:
+        if period in col:
+            fig.add_trace(go.Scatter(x=trades.index.to_list(), y=trades[('Balance', col)].to_list(), name=col.replace('_', ' ')))
 st.plotly_chart(fig, use_container_width=True)
+
+stats = pd.DataFrame()
+s = trades.ffill().iloc[-1]
+for metric, period in trades.columns:
+    for p in ['Globally', 'Yearly', 'Monthly', 'Weekly']:
+        if p in period and metric in ['Count', 'Balance', 'CountTP', 'RateTP', 'PayoffTP', 'CountBE', 'RateBE', 'PayoffBE', 'CountSL', 'RateSL', 'PayoffSL']:
+            stats.loc[period, metric] = s[(metric, period)]
+stats['WinRate'] = stats['RateTP']
+stats['PayoffRatio'] = stats['PayoffTP'] / -stats['PayoffSL']
